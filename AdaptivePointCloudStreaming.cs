59aa6a0f563617e50d14035f2042c58c24e44531
KazumaMori <negitoro583@gmail.com>
2022-12-19 15:43:37 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   AdaptivePointCloudStreaming.cs

--- AdaptivePointCloudStreaming.cs
+++ AdaptivePointCloudStreaming.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 using System;
-using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+//Need Using XElement
+using System.Xml.Linq;
 
 public class AdaptivePointCloudStreaming : MonoBehaviour
 {
 /*-------------------------    Parameters   -----------------------------*/
+    //Server Info
+    public string xmlURL = "http://172.16.51.34/test.xml";
+
     //Queue
-    public int bufferSize = 5;
+    public int bufferSize = 1;
     Queue<Vector3[]> pointsQue;
     Queue<Color32[]> colorsQue;
     Queue<int[]> indicesPointsQue;
@@ -25,29 +29,73 @@ public class AdaptivePointCloudStreaming : MonoBehaviour
     int[] representationIdArray;
     string[] urlArray;
     int[] sumPointsArray;
+    double[] processingTimeArray;
 
     //HTTP Thread
     private bool flagLoop = true;
     private bool xmlStatus = false;
+    public int nextRepresentationId = 0;
 
     //Network manager
     string serverPointCloudString;
+    public int joinBuffer = 10;
+    public bool joinBufferFlag = false;
+    public int joinFrameFlag = 100;
+    public int joinFrame = 0;
+    int bufferingFrame;
+    // int bufferingRatio;
+    // int bufferingEvent;
+
     //Visualizer
+    // private bool playStatus = false;
     public MeshFilter meshFilter;
 
 /*----------------------------    Play   --------------------------------*/
     // Start is called before the first frame update
     void Start()
     {
-        meshFilter = GetComponent<MeshFilter>();
+        Application.targetFrameRate = 1;
+        Debug.Log($"Main thread ID : " + Thread.CurrentThread.ManagedThreadId);
+        Debug.Log("Start!!!");
+
         StartCoroutine("GetXmlRequest", xmlURL);
-        HTTPThread();
+        CreateQue();
+        // HTTPThread();
+        meshFilter = GetComponent<MeshFilter>();
+        Debug.Log("--------- Start() Function Finish --------------");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        ShowPointCloud();
+        if(xmlStatus && joinBufferFlag){
+            Debug.Log("--------- Update Start --------------");    
+            ShowPointCloud();
+            Debug.Log("--------- Update Finish --------------");
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        if(xmlStatus){
+            if(nextRepresentationId == 6 && !joinBufferFlag){
+                if(pointsQue.Count == joinBuffer){
+                    joinBufferFlag = true;
+                }
+            }else{
+                Debug.Log("-----FixedUpdate start------------nowId :" + nextRepresentationId + " Now Buffer Size : " + pointsQue.Count + " xml Status : " + xmlStatus +" :" + (pointsQue.Count < bufferSize));
+                string urlPointCloudTxt = urlArray[nextRepresentationId];
+                int sumPoints = sumPointsArray[nextRepresentationId];
+                int representationId =representationIdArray[nextRepresentationId];
+
+                // StartCoroutine("GetPointCloud", (representationId, sumPoints, urlPointCloudTxt));
+                StartCoroutine(GetPointCloud(representationId, sumPoints, urlPointCloudTxt));
+                nextRepresentationId++;
+                Debug.Log("-----FixedUpdate Finish --------------");
+            }
+        }
     }
 
 /*-------------------------   HTTP Thread   ----------------------------*/
@@ -61,8 +109,16 @@ public class AdaptivePointCloudStreaming : MonoBehaviour
         {
             while(flagLoop){
                 try{
-                    if(pointsQue.Count < bufferSize){
-                        StartCoroutine("GetPointCloud", urlPointCloudTxt);
+                    Debug.Log("----------HTTPThread Thread ID : " + Thread.CurrentThread.ManagedThreadId +"--------");
+
+                    if( xmlStatus && (pointsQue.Count < bufferSize)){
+                        string urlPointCloudTxt = urlArray[nextRepresentationId];
+                        int sumPoints = sumPointsArray[nextRepresentationId];
+                        int representationId =representationIdArray[nextRepresentationId];
+
+                        StartCoroutine(GetPointCloud(representationId, sumPoints, urlPointCloudTxt));
+                        nextRepresentationId++;
+                        Debug.Log("--------- HTTPThread Finish --------------");
                     }else{
                         //All store in a buffer
                     }
@@ -78,8 +134,7 @@ public class AdaptivePointCloudStreaming : MonoBehaviour
 /*-----------------------   Network Manager   --------------------------*/
     IEnumerator GetXmlRequest(string url)
     {
-        Debug.Log("---------------------------------------------------------------------");
-        Debug.Log("Load xml start");
+        Debug.Log("---------------------------------Load xml start------------------------------------");
         var sw = new System.Diagnostics.Stopwatch();
         var swRTT = new System.Diagnostics.Stopwatch();
         sw.Start();
@@ -98,41 +153,42 @@ public class AdaptivePointCloudStreaming : MonoBehaviour
             XElement xml = XElement.Parse(webRequest.downloadHandler.text);
             IEnumerable<XElement> xelements = xml.Elements("Representation");
 
-            frameCounts = int.Parse(xml.Attribute("frameCounts").Value);
+            frameCounts = int.Parse(xml.Attribute("FrameCounts").Value);
             sumPointsArray = new int[frameCounts];
             urlArray = new string[frameCounts];
             processingTimeArray = new double[frameCounts];
+            representationIdArray = new int[frameCounts];
 
-            Debug.Log(frameCounts);
-            int index = 0;
             foreach (XElement xelement in xelements){
+                int index = int.Parse(xelement.Element("id").Value);
                 sumPointsArray[index] = int.Parse(xelement.Element("NumPoints").Value);
-                int id = int.Parse(xelement.Element("id").Value);
+                representationIdArray[index] = index;
                 urlArray[index] = xelement.Element("BaseURL").Value;
 
                 //Debug last parameter in test
                 if(index == frameCounts-1){
-                    Debug.Log("id : " + id + "   Load sumPoints:" + sumPointsArray[index] + "   BaseURL:" + urlArray[index]);
+                    Debug.Log(" Loaded XML id : " + representationIdArray[index] + "   Load sumPoints:" + sumPointsArray[index] + "   BaseURL:" + urlArray[index]);
                 }
-
-                index++;
             }
-
+            xmlStatus = true;
             sw.Stop();
             Debug.Log("Load xml finish");
-            Debug.Log("Total processing time" + sw.Elapsed.TotalMilliseconds +"ms  RTT:" + swRTT.Elapsed.TotalMilliseconds +"ms");
-            Debug.Log("---------------------------------------------------------------------");
+            Debug.Log("FrameCounts :" + frameCounts + "  Total processing time" + sw.Elapsed.TotalMilliseconds +"ms  RTT:" + swRTT.Elapsed.TotalMilliseconds +"ms" + "  xml Status : " + xmlStatus);
+            Debug.Log("---------------------------------End Get xml Request------------------------------------");
         }
     }
 
-    IEnumerator GetPointCloud(string urlPointCloudTxt)
+    IEnumerator GetPointCloud(int representationId, int sumPoints, string urlPointCloudTxt)
     {
-        Debug.Log("---------------------------------------------------------------------");
-        Debug.Log("Get Point Cloud Text start");
+        int sumPointsNow = sumPoints;
+        int representationIdNow = representationId;
+        Debug.Log("--------------------------------------GetPointCloud-------------------------------urlPointCloudTxt : " + urlPointCloudTxt);
+        // Debug.Log("Get Point Cloud Text start");
         var sw = new System.Diagnostics.Stopwatch();
         var swRTT = new System.Diagnostics.Stopwatch();
         sw.Start();
         swRTT.Start();
+
         //Prepare Get by urlPointCloudTxt
         UnityWebRequest webRequest = UnityWebRequest.Get(urlPointCloudTxt);
         yield return webRequest.SendWebRequest();
@@ -146,34 +202,35 @@ public class AdaptivePointCloudStreaming : MonoBehaviour
             string serverPointCloudString = webRequest.downloadHandler.text;
             
             //Task.ConvertToPointCloud
-            var ConvertTask = Task.Run(() => ConvertToPointCloud(sumPoints, serverPointCloudString));
+            Task.Run(() => ConvertToPointCloud(representationIdNow, sumPointsNow, serverPointCloudString));
         }
         sw.Stop();
-        Debug.Log("Get Point Cloud Text End");
-        Debug.Log("Total processing time" + sw.Elapsed.TotalMilliseconds +"ms  RTT:" + swRTT.Elapsed.TotalMilliseconds +"ms");
-        Debug.Log("---------------------------------------------------------------------");
+        // Debug.Log("Get Point Cloud Text End");
+        Debug.Log(" GetPointCloud Total processing time" + sw.Elapsed.TotalMilliseconds +"ms  RTT:" + swRTT.Elapsed.TotalMilliseconds +"ms URL: " + urlPointCloudTxt );
+        Debug.Log("---------------------------End GetPointcloud------------------------------------------");
     }
     
-
 /*----------------------------   Queue   -------------------------------*/
     private void CreateQue(){
         pointsQue = new Queue<Vector3[]>(bufferSize);
         colorsQue = new Queue<Color32[]>(bufferSize);
         indicesPointsQue = new Queue<int[]>(bufferSize);
+        representationIdQue = new Queue<int>(bufferSize);
     }
 
-    private void ConvertToPointCloud(int sumPoints, string serverPointCloudString){
-        Debug.Log("Convert to Point Cloud now......")
+    private void ConvertToPointCloud(int representationIdNow, int sumPoints, string serverPointCloudString){
+        Debug.Log("------------------ Convert to Point Cloud now......  ---representationIdNow : " + representationIdNow +   "   FixedUpdate Thread ID : " + Thread.CurrentThread.ManagedThreadId);
+
         var swConvertToPointCloud = new System.Diagnostics.Stopwatch();
         swConvertToPointCloud.Start();
-        Debug.Log($"task contents : {Thread.CurrentThread.ManagedThreadId}");
+
         char[] del = {'\n', ' '};
         string[] rawPointsArray = serverPointCloudString.Split(del);
 
         int[] indices = new int[sumPoints];
         Vector3[] points = new Vector3[sumPoints];
         Color32[] colors = new Color32[sumPoints];
-
+        Debug.Log("convert Now");
         //もっとパラレル合理化したい　foreach?
         Parallel.For(0, sumPoints, i =>{
             int j = i*7;
@@ -190,19 +247,27 @@ public class AdaptivePointCloudStreaming : MonoBehaviour
                 (byte)int.Parse(rawPointsArray[j+6])
                 );
         });
+        Debug.Log("-------------------------------------------------------" + points[0]);
+        Debug.Log(colors[0]);
 
     //Insert point cloud to Queue
         pointsQue.Enqueue(points);
         colorsQue.Enqueue(colors);
         indicesPointsQue.Enqueue(indices);
+        representationIdQue.Enqueue(representationIdNow);
 
         swConvertToPointCloud.Stop();
-        Debug.Log("Finish Convert!!!" +"   Convert processing time : " + swConvertToPointCloud.Elapsed.TotalMilliseconds);
+        Debug.Log("------------------------------Finish Convert!!!" +"  Total convert processing time : " + swConvertToPointCloud.Elapsed.TotalMilliseconds +"  Buffer itme :" + pointsQue.Count + "              FixedUpdate Thread ID : " + Thread.CurrentThread.ManagedThreadId);
     }
 
 /*--------------------------   Visualizer   -----------------------------*/
     private void ShowPointCloud(){
-        if(pointsQue.Count >0 && colorsQue.Count > 0 && indicesPointsQue.Count > 0){
+        if(pointsQue.Count == 0){
+            if(frameCounts == nextRepresentationId){
+                Debug.Log("----------------------------------End Point Cloud Video!!!!!!!!!-----------------------------------");
+            }
+            Debug.Log("Queue is empty!!!");
+        }else{
             Mesh mesh = new Mesh();
             mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
 
@@ -213,8 +278,7 @@ public class AdaptivePointCloudStreaming : MonoBehaviour
 
             mesh.name = "PointsCloudMesh";  
             meshFilter.mesh = mesh;
-        }else{
-            Debug.Log("Queue is empty!!!");
+            Debug.Log("Now buffer frame : " + pointsQue.Count + "Representation Id : " + representationIdQue.Dequeue());
         }
     }
 
